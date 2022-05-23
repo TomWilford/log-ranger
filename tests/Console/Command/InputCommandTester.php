42980b6f51b4ce7e934c13a44f9b1c03b3eee21a
Tom Wilford <hello@jollyblueman.com>
2022-05-23 20:27:04 +0100

    tidying

--- tests/Console/Command/InputCommandTester.php
+++ tests/Console/Command/InputCommandTester.php
@@ -3,7 +3,6 @@
 namespace Wilf\Tests\Console\Command;
 
 use PHPUnit\Framework\TestCase;
-use Wilf\Console\Command\GreetCommand;
 use Wilf\Console\Command\InputCommand;
 use Zenstruck\Console\Test\TestCommand;
 
